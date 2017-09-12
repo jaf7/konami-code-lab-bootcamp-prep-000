80ca53b88a1658dc3fa1eeb30ae53d5e41dfeae9
Anthony Fields <janthonyfields@gmail.com>
2017-09-12 19:44:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+let indexCounter = 0;
 function init() {
   // Write your JavaScript code inside the init() function
   console.log('init() loaded');
   document.body.addEventListener( 'keydown', codeCheck );
 
   function codeCheck(e) {
-    let indexCounter = 0;
     let keyPressed = parseInt( e.which || e.detail );
     console.log('keyPressed: ', keyPressed);
     console.log('indexCounter: ', indexCounter);

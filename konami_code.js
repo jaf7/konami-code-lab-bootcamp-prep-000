4b3a313093d5c0a070a724a57cda1e75049512a6
Anthony Fields <janthonyfields@gmail.com>
2017-09-12 19:30:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,25 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  console.log('init() loaded');
+  document.body.addEventListener( 'keydown', codeCheck );
 
+  function codeCheck(e) {
+    let indexCounter = 0;
+    let keyPressed = parseInt( e.which || e.detail );
+    console.log('keyPressed: ', keyPressed);
+    console.log('indexCounter: ', indexCounter);
+    console.log('code[indexCounter]: ', code[indexCounter]);
+      debugger
+    if ( keyPressed === code[indexCounter] ) {
+      indexCounter++;
+      // console.log('indexCounter: ', indexCounter);
+
+      if ( indexCounter.length === code.length ) {
+        alert( 'Konami!' );
+      }
+    } else {
+      indexCounter = 0;
+    }
+  }
 }
